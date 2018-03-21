84ec126ccb8c3a6c483fea3ba5e9fc8f35723e98
Eric Baukhages <febvigrail@gmail.com>
2018-03-20 22:21:02 -0400

    init

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	flag "github.com/ogier/pflag"
+)
+
+var (
+	session string
+)
+
+func main() {
+	flag.Parse()
+
+	if flag.NFlag() == 0 {
+		printUsage()
+		os.Exit(1)
+	}
+
+	fmt.Printf("Loading session: %s\n", session)
+}
+
+func init() {
+	flag.StringVarP(&session, "session", "s", "", "Session")
+}
+
+func printUsage() {
+	fmt.Printf("Usage: %s [options]\n", os.Args[0])
+	fmt.Println("Options:")
+	flag.PrintDefaults()
+}
